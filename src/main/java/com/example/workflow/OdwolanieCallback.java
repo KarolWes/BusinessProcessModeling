943e4bfb90a72597bab6c5ff8f2ba502c51b61d3
KarolWes <karol.wesolowski01@gmail.com>
2024-10-20 17:21:29 +0200

    lab 5 - complete

---
+++ src/main/java/com/example/workflow/OdwolanieCallback.java
@@ -0,0 +1,17 @@
+package com.example.workflow;
+
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class OdwolanieCallback implements JavaDelegate {
+    public void execute(DelegateExecution execution) throws Exception {
+        RuntimeService runtimeService =
+                execution.getProcessEngineServices().getRuntimeService();
+        runtimeService.createMessageCorrelation("wynikOdwolaniaMsg")
+                .processInstanceId(execution.getVariable("parentBussinesKey").toString())
+                .setVariable("decyzja_czyPozytywna", execution.getVariable("decyzja_czyPozytywna"))
+                .setVariable("decyzja_uzasadnienie", execution.getVariable("decyzja_uzasadnienie"))
+                .correlateWithResult();}
+}
+
